f429fb50a91189cb01c86c2638fbc54b117e1ae7
Ritesh Bangal <riteshbangal@gmail.com>
2018-01-21 21:58:46 +0530

    Changes for Camel REST Consumer

--- camel-rest-consumer/src/main/java/com/poc/rest/consumer/MyProcessor.java
+++ camel-rest-consumer/src/main/java/com/poc/rest/consumer/MyProcessor.java
@@ -6,6 +6,17 @@ import org.apache.camel.Processor;
 public class MyProcessor implements Processor {
 
 	public void process(Exchange exchange) throws Exception {
+
+		System.out.println("Process started...");
+		
+		// Extracting information from the request
+		String incomingText = exchange.getIn().getBody(String.class);
+		if (null == incomingText) {
+			System.out.println("Not able to Processed successfully, as incoming text is empty.");
+			return;
+		}
+		
+		System.out.println("Request Text :: " + incomingText);
 		System.out.println("Processed successfully");
 	}
 }
--- camel-rest-consumer/src/main/java/com/poc/rest/consumer/MyRouteBuilder.java
+++ camel-rest-consumer/src/main/java/com/poc/rest/consumer/MyRouteBuilder.java
@@ -1,13 +1,49 @@
 package com.poc.rest.consumer;
 
+import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
 
 public class MyRouteBuilder extends RouteBuilder {
 
 	public void configure() {
 
-		from("file:D:/tmp/inputFolder/catalog")
+		//restConfiguration().component("http").host("localhost").port(9000);
+		
+		/*rest("/service/topics")
+        .get().consumes("application/json")
+		//.process(new MyProcessor())
+		.to("file:D:/tmp/outputFolder/catalog");*/
+		
+		from("timer:service/topics?period=2000")
+		//from("quartz2://httpFetch?cron=* * * * * ? *")
+		//from("direct:start")
+		//.process(new MyProcessor())
+		
+		.setHeader(Exchange.HTTP_METHOD, constant(org.apache.camel.component.http4.HttpMethods.POST))
+		//.setHeader(Exchange.CONTENT_TYPE, constant("application/xml"))
+		//.to("http4://localhost:9000/service/topics?throwExceptionOnFailure=false").streamCaching()
+		.to("http4://localhost:9000/service/topics/100?throwExceptionOnFailure=false&authUsername=admin&authPassword=admin").streamCaching()
+		
 		.process(new MyProcessor())
 		.to("file:D:/tmp/outputFolder/catalog");
+		
+		
+		/*from("http:localhost:9000/service/topics")
+		.setHeader(Exchange.HTTP_METHOD, constant(org.apache.camel.component.http4.HttpMethods.GET))
+		.process(new MyProcessor())
+		.to("file:D:/tmp/outputFolder/catalog");*/
+		
+		//from("direct:getRestExternalService") 
+		/*from("direct:start")
+		.setHeader(Exchange.HTTP_METHOD, constant(org.apache.camel.component.http4.HttpMethods.GET))
+			.process(new MyProcessor())
+			.to("http4://localhost:9000/service/topics")
+			.process(new MyProcessor())
+			.to("file:D:/tmp/outputFolder/catalog");*/
+		
+		 /*from("http4:localhost:9000/service/topics")
+		 	.process(new MyProcessor())
+		 	.to("file:D:/tmp/outputFolder/catalog");
+         	//.to("direct:getTopics");*/
 	}
 }
